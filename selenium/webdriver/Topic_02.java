25ddc4e1d732c8efd4e8e8a07da1b95dc6d3c2bb
selenium <nhungquyen98@gmail.com>
2024-01-21 22:44:13 +0700

    commit  21/01 v2

--- selenium/webdriver/Topic_02.java
+++ selenium/webdriver/Topic_02.java
@@ -1,4 +1,13 @@
 package webdriver;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Topic_02 {
+    @Test
+    public void TC_02_Logo() {
+        Assert.assertTrue(driver.findElement(By.cssSelector("img.fb_logo")).isDisplayed());
+    }
+
 }
